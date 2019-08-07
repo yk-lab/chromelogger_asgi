91bc6c743c0bf23ab77884e6206e10a88cb83506
YetAnother_yk <www.yklab.net@gmail.com>
2019-08-07 14:03:33 +0900

    first commit

---
+++ chromelogger_asgi/middleware.py
@@ -0,0 +1,12 @@
+class ChromeLoggerMiddleware:
+    def __init__(self, app, console):
+        self.app = app
+        self.console = console
+
+    async def __call__(self, scope, receive, send):
+        headers = dict(scope["headers"])
+        if scope["type"] in ("http"):
+            header = console.get_header()
+            if header is not None:
+                headers.update(header)
+        await self.app(scope, receive, send)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    packages=['chromelogger_asgi'],
+    use_scm_version=True,
+    setup_requires=[
+        "setuptools_scm"
+    ]
+)
