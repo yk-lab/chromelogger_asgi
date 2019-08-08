f15ef0bb64e5059beef17890cd38c1f240c4f271
YetAnother_yk <www.yklab.net@gmail.com>
2019-08-08 19:10:39 +0900

    refactoring

--- chromelogger_asgi/middleware.py
+++ chromelogger_asgi/middleware.py
@@ -1,25 +1,13 @@
-import logging
-
 import chromelogger as console
-
-logger = logging.getLogger()
-
 
 class ChromeLoggerMiddleware:
     def __init__(self, app):
-        logger.debug("ChromeLoggerMiddleware __init__")
-        logger.debug(f"app type is {type(app)}")
         self.app = app
 
     async def __call__(self, scope, receive, send):
-        logger.debug("ChromeLoggerMiddleware __call__")
-        logger.debug(f"scope is {scope}, type: {type(scope)}")
-        headers = dict(scope["headers"])
-        logger.debug(f"headers is {headers}")
+        # headers = dict(scope["headers"])
         if scope["type"] in ("http"):
             header = console.get_header()
-            logger.debug(f"console header is {header}")
-            if header is not None:
-                headers.update(header)
-                logger.debug(f"updated headers")
+            # if header is not None:
+            #     headers.update({header[0]: header[1]})
         await self.app(scope, receive, send)
