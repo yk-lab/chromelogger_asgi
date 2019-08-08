b04688a3979a073549d8cda78fa7515eb769e913
YetAnother_yk <www.yklab.net@gmail.com>
2019-08-08 15:49:47 +0900

    try

--- chromelogger_asgi/middleware.py
+++ chromelogger_asgi/middleware.py
@@ -4,10 +4,10 @@ class ChromeLoggerMiddleware:
     def __init__(self, app):
         self.app = app
 
-    async def __call__(self, scope):
+    async def __call__(self, scope, receive, send):
         headers = dict(scope["headers"])
         if scope["type"] in ("http"):
             header = console.get_header()
             if header is not None:
                 headers.update(header)
-        await self.app(scope)
+        await self.app(scope, receive, send)
